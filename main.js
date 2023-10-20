1d1d944d0d03a8ef201665feb12f49a585193269
Chris Nelson <chrisnelson7791@gmail.com>
2023-10-19 17:03:06 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function smash (words) {
+  return words.join(' ')
+};
+
+console.log(smash(['hello', 'general', 'kenobi']))
+
+// parameters: words     an array of words
+// return: sentence    a string made up of the words in the param array
+// e.g. smash(['hello', 'general', 'kenobi'])
+// results in 'hello general kenobi'
+
+
+// return words.join(' ') where the separator is a single space
